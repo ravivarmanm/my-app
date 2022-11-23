107c66e4bbb02104a1e0e9cbffb9634147303bc9
Ravivarman M <ravivarmanm@outlook.com>
2022-11-23 13:43:05 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Greet from "./Components/Greet";
+import Welcome from "./Components/welcome";
+import Hello from "./Components/Hello";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="bruce" heroname="hulk">
+        <p>this is children props</p>
+        <h3>Ravivarman</h3>
+      </Greet>
+      <Greet name="clark" heroname="superman">
+        <button className="btn btn-primary">Action</button>
+      </Greet>
+      <Greet name="tony" heroname="IronMan" />
+      <Welcome name="bruce" heroname="hulk">
+        {" "}
+      </Welcome>
+      <Welcome name="clark" heroname="superman">
+        {" "}
+      </Welcome>
+      <Welcome name="tony" heroname="IronMan">
+        {" "}
+      </Welcome>
+      <Hello name="toons"></Hello>
     </div>
   );
-}
+} 
 
 export default App;
---
+++ src/Components/Hello.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Hello = () => {
+  //   return (
+  //     <div>
+  //       <h1>Hello World!</h1>
+  //     </div>
+  //   );
+  return React.createElement(
+    "div",
+    { id: 1, className: "dummy" },
+    React.createElement("h1", null, "Hello")
+  );
+};
+export default Hello;
